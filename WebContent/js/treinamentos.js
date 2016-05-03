e2479e5c04dbc49b1f805fcc6d1d8c7e1d5d8aa5
Felipe Leme <lemefe21@gmail.com>
2016-05-02 21:21:19 -0300

    Termino do curso de introdução ao JQuery com criação e estilização de elementos dinamicamente, animações e requisições Ajax

--- WebContent/js/treinamentos.js
+++ WebContent/js/treinamentos.js
@@ -1,13 +1,31 @@
-$('#lista').on("click", "li", function(){
+$('#lista').on("dblclick", "li", function(){
 	
-	var texto = $(this).text();
-	alert(texto);
+	$(this).animate({'margin-left':'+=200'}, function(){
+		
+		$(this).remove();
+		
+	});
 	
 });
 
 $('#botao-adiciona').click(function(){
 	
 	var texto = $('#treinamento').val();
-	$('<li>').text(texto).appendTo('#lista');
+	$('<li>').text(texto).css({'color':'blue', 'background-color':'lightgrey'}).appendTo('#lista');
 	
+});
+
+$('#botao-atualiza').click(function(){
+	
+	$.get('http://mirrorfashion.caelum.com.br/treinamentos', function(retorno){
+		
+		$.each(retorno.treinamentos, function(){
+			
+			var treinamento = this;
+			$('<li>').css('color', 'green').text(treinamento.nome).appendTo('#lista');
+			
+		});
+		
+	}, 'jsonp');
+
 });
